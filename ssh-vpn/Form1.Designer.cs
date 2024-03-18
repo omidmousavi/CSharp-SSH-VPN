950a650cb5b7186b161e7e3e239572c1e6bb0a09
motb <redboy5093@gmail.com>
2024-03-18 08:29:30 +0330

    updated ui, added multithreding for opening/closing connection

--- ssh-vpn/Form1.Designer.cs
+++ ssh-vpn/Form1.Designer.cs
@@ -30,86 +30,52 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnToggle = new System.Windows.Forms.Button();
             this.githubLink = new System.Windows.Forms.LinkLabel();
-            this.btnDisconnect = new System.Windows.Forms.Button();
-            this.pnlStatus = new System.Windows.Forms.Panel();
-            this.lblStatus = new System.Windows.Forms.Label();
-            this.opensettingsform = new System.Windows.Forms.Button();
+            this.btnOpenSettings = new System.Windows.Forms.Button();
             this.timer_check_status = new System.Windows.Forms.Timer(this.components);
             this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
-            this.pnlStatus.SuspendLayout();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnGh = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // btnConnect
+            // btnToggle
             // 
-            this.btnConnect.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnConnect.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnConnect.Location = new System.Drawing.Point(12, 14);
-            this.btnConnect.Name = "btnConnect";
-            this.btnConnect.Size = new System.Drawing.Size(260, 50);
-            this.btnConnect.TabIndex = 0;
-            this.btnConnect.Text = "Connect";
-            this.btnConnect.UseVisualStyleBackColor = true;
-            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            this.btnToggle.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnToggle.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnToggle.Location = new System.Drawing.Point(12, 14);
+            this.btnToggle.Name = "btnToggle";
+            this.btnToggle.Size = new System.Drawing.Size(260, 83);
+            this.btnToggle.TabIndex = 0;
+            this.btnToggle.Text = "Connect";
+            this.btnToggle.UseVisualStyleBackColor = true;
+            this.btnToggle.Click += new System.EventHandler(this.btnToggle_Click);
             // 
             // githubLink
             // 
             this.githubLink.AutoSize = true;
-            this.githubLink.Location = new System.Drawing.Point(12, 208);
+            this.githubLink.Location = new System.Drawing.Point(12, 256);
             this.githubLink.Name = "githubLink";
             this.githubLink.Size = new System.Drawing.Size(65, 13);
             this.githubLink.TabIndex = 2;
             this.githubLink.TabStop = true;
             this.githubLink.Text = "Github page";
-            this.githubLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.githubLink_LinkClicked);
+            this.githubLink.Visible = false;
             // 
-            // btnDisconnect
+            // btnOpenSettings
             // 
-            this.btnDisconnect.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnDisconnect.Enabled = false;
-            this.btnDisconnect.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnDisconnect.Location = new System.Drawing.Point(12, 70);
-            this.btnDisconnect.Name = "btnDisconnect";
-            this.btnDisconnect.Size = new System.Drawing.Size(260, 50);
-            this.btnDisconnect.TabIndex = 3;
-            this.btnDisconnect.Text = "Disconnect";
-            this.btnDisconnect.UseVisualStyleBackColor = true;
-            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
-            // 
-            // pnlStatus
-            // 
-            this.pnlStatus.BackColor = System.Drawing.Color.Red;
-            this.pnlStatus.Controls.Add(this.lblStatus);
-            this.pnlStatus.Location = new System.Drawing.Point(12, 126);
-            this.pnlStatus.Name = "pnlStatus";
-            this.pnlStatus.Size = new System.Drawing.Size(260, 46);
-            this.pnlStatus.TabIndex = 4;
-            // 
-            // lblStatus
-            // 
-            this.lblStatus.AutoSize = true;
-            this.lblStatus.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblStatus.ForeColor = System.Drawing.Color.White;
-            this.lblStatus.Location = new System.Drawing.Point(67, 12);
-            this.lblStatus.Name = "lblStatus";
-            this.lblStatus.Size = new System.Drawing.Size(124, 21);
-            this.lblStatus.TabIndex = 0;
-            this.lblStatus.Text = "Not connected";
-            // 
-            // opensettingsform
-            // 
-            this.opensettingsform.Location = new System.Drawing.Point(157, 200);
-            this.opensettingsform.Name = "opensettingsform";
-            this.opensettingsform.Size = new System.Drawing.Size(115, 29);
-            this.opensettingsform.TabIndex = 1;
-            this.opensettingsform.Text = "Open settings";
-            this.opensettingsform.UseVisualStyleBackColor = true;
-            this.opensettingsform.Click += new System.EventHandler(this.opensettingsform_Click);
+            this.btnOpenSettings.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnOpenSettings.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.btnOpenSettings.Location = new System.Drawing.Point(73, 178);
+            this.btnOpenSettings.Name = "btnOpenSettings";
+            this.btnOpenSettings.Size = new System.Drawing.Size(199, 51);
+            this.btnOpenSettings.TabIndex = 1;
+            this.btnOpenSettings.Text = "Open Settings";
+            this.btnOpenSettings.UseVisualStyleBackColor = true;
+            this.btnOpenSettings.Click += new System.EventHandler(this.btnOpenSettings_Click);
             // 
             // timer_check_status
             // 
-            this.timer_check_status.Enabled = true;
             this.timer_check_status.Interval = 1000;
             this.timer_check_status.Tick += new System.EventHandler(this.timer_check_status_Tick);
             // 
@@ -118,39 +84,62 @@
             this.notifyIcon1.Text = "SSH VPN NOTIF";
             this.notifyIcon1.Visible = true;
             // 
+            // lblStatus
+            // 
+            this.lblStatus.BackColor = System.Drawing.Color.Red;
+            this.lblStatus.Cursor = System.Windows.Forms.Cursors.Default;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F);
+            this.lblStatus.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblStatus.Location = new System.Drawing.Point(12, 100);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(260, 75);
+            this.lblStatus.TabIndex = 6;
+            this.lblStatus.Text = "Not Connected";
+            this.lblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnGh
+            // 
+            this.btnGh.BackColor = System.Drawing.SystemColors.ControlText;
+            this.btnGh.BackgroundImage = global::ssh_vpn.Properties.Resources.gh;
+            this.btnGh.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnGh.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnGh.FlatAppearance.BorderSize = 0;
+            this.btnGh.Location = new System.Drawing.Point(12, 178);
+            this.btnGh.Name = "btnGh";
+            this.btnGh.Size = new System.Drawing.Size(55, 51);
+            this.btnGh.TabIndex = 5;
+            this.btnGh.UseVisualStyleBackColor = false;
+            this.btnGh.Click += new System.EventHandler(this.btnGh_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(281, 239);
-            this.Controls.Add(this.opensettingsform);
-            this.Controls.Add(this.pnlStatus);
-            this.Controls.Add(this.btnDisconnect);
+            this.ClientSize = new System.Drawing.Size(281, 241);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnGh);
+            this.Controls.Add(this.btnOpenSettings);
             this.Controls.Add(this.githubLink);
-            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.btnToggle);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "SSH VPN";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
-            this.pnlStatus.ResumeLayout(false);
-            this.pnlStatus.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btnConnect;
         private System.Windows.Forms.LinkLabel githubLink;
-        private System.Windows.Forms.Button btnDisconnect;
-        private System.Windows.Forms.Panel pnlStatus;
-        private System.Windows.Forms.Label lblStatus;
-        private System.Windows.Forms.Button opensettingsform;
+        private System.Windows.Forms.Button btnOpenSettings;
         private System.Windows.Forms.Timer timer_check_status;
         private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Button btnGh;
+        private System.Windows.Forms.Label lblStatus;
+        public System.Windows.Forms.Button btnToggle;
     }
 }
 
--- ssh-vpn/Properties/Resources.Designer.cs
+++ ssh-vpn/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace ssh_vpn.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -57,6 +57,36 @@ namespace ssh_vpn.Properties {
             }
             set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap gh {
+            get {
+                object obj = ResourceManager.GetObject("gh", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap vpn_icon {
+            get {
+                object obj = ResourceManager.GetObject("vpn-icon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap vpn_icon_2 {
+            get {
+                object obj = ResourceManager.GetObject("vpn-icon-2", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
     }
